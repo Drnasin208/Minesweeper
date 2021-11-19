3ea968c1355eb8515379aa4aa867bba86b12d4b7
Drnasin208 <60597312+Drnasin208@users.noreply.github.com>
2021-11-19 14:16:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -149,7 +149,7 @@ document.addEventListener('DOMContentLoaded', () => {
         //show all the Bombs
         squares.forEach(square => {
             if (square.classList.contains('bomb')) {
-                square.innerHTML = '💣'
+                square.innerHTML = '💣'<a href="https://giphy.com/stickers/glitchers-bomb-tnt-exploding-xL7FqDrpmKhS4ALfmo">via GIPHY</a>
             }
         })
     }
